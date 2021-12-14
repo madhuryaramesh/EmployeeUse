0c20f50a94fbcd86eb51449a095364e09601b179
madhurya <madhuryar017@gmail.com>
2021-12-14 21:11:45 +0530

    [madhurya] Adding UseCase2

--- EmployeeUse/Program.cs
+++ EmployeeUse/Program.cs
@@ -7,8 +7,13 @@ namespace EmployeeUse
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            UCase1 uCase = new UCase1();
-            uCase.UCase1Method();
+            //UCase1 uCase = new UCase1();
+            //uCase.UCase1Method();
+
+            //Use Case 2
+            UCase2 uCase2 = new UCase2();
+            uCase2.UCase2Method();
+                
         }
     }
 }
---
+++ EmployeeUse/UCase2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeUse
+{
+    public class UCase2
+    {
+        public void UCase2Method()
+        {
+            int Is_Fulltime = 1;
+            int Emp_Rate_PerHour = 20;
+
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == Is_Fulltime)
+            {
+                emphrs=8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * Emp_Rate_PerHour;
+            Console.WriteLine("Employee Wage is " + empwage);
+        }
+    }
+}
