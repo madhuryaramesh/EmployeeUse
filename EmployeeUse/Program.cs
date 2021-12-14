5160b3c0f89e06a94c308b435973c41dd8a84041
madhurya <madhuryar017@gmail.com>
2021-12-14 20:55:52 +0530

    [madhurya] Adding master

---
+++ EmployeeUse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //UCase1 uCase = new UCase1();
+            //uCase.UCase1Method();
+        }
+    }
+}
---
+++ EmployeeUse/UCase1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeUse
+{
+    public class UCase1
+    {
+        public void UCase1Method()
+        {
+
+            //int Is_Fulltime = 1;
+            //Random random = new Random();
+            //int empcheck = random.Next(0, 2);
+            //if (empcheck == Is_Fulltime)
+            //{
+            //    Console.WriteLine("PRESENT");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ABSENT");
+            //}
+
+        }
+    }
+}
