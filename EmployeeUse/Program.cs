f9bb3214cd5b4bf1c0aad77410b6187a5d6e251a
madhurya <madhuryar017@gmail.com>
2021-12-15 16:31:59 +0530

    [madhurya] Adding UC3

--- EmployeeUse/Program.cs
+++ EmployeeUse/Program.cs
@@ -6,13 +6,9 @@ namespace EmployeeUse
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //UCase1 uCase = new UCase1();
-            //uCase.UCase1Method();
-
-            //Use Case 2
-            UCase2 uCase2 = new UCase2();
-            uCase2.UCase2Method();
+            
+            UseCase uCase2 = new UseCase();
+            uCase2.UCaseMethod();
                 
         }
     }
---
+++ EmployeeUse/UseCase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeUse
+{
+    public class UseCase
+    {
+        public const int isParttime = 1;
+        public const int isFulltime = 2;
+        public const int empRateHrs = 20;
+        public void UCaseMethod()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+            if (empCheck == isParttime)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == isFulltime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            Console.WriteLine("UC3");
+            Console.WriteLine("-------------------------------");
+            empWage = empHrs * empRateHrs;
+            Console.WriteLine("Emp Wage is : " + empWage);
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
