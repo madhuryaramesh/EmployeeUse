e0098660e6c96b06b5a714f33c8f6a9fa16f2baa
madhurya <madhuryar017@gmail.com>
2021-12-22 11:08:47 +0530

    [madhurya] Adding UC8

--- EmployeeUse/Program.cs
+++ EmployeeUse/Program.cs
@@ -6,58 +6,54 @@ namespace EmployeeUse
     {
         public const int isParttime = 1;
         public const int isFulltime = 2;
-        public const int empRateHrs = 20;
-        public const int numWrkDays = 2;
-        public const int maxHrMnth = 10;
-        public static int UCaseMethod()
+
+        public static int UCEwage(string company, int empRateHrs, int numWrkDays, int maxHrMnth)
         {
             int empHrs = 0;
-            int empWage = 0;
             int totalEmpWage = 0;
             int totalEmpHrs = 0;
             int totalWrkDays = 0;
-            Console.WriteLine("UC7");
+            Console.WriteLine("UC8");
             Console.WriteLine("-------------------------------");
             while (totalEmpHrs <= maxHrMnth && totalWrkDays < numWrkDays)
             {
                 totalWrkDays++;
                 Random rand = new Random();
                 int empCheck = rand.Next(0, 3);
-
+               
                 switch (empCheck)
                 {
                     case isParttime:
                         empHrs = 4;
                         break;
+
                     case isFulltime:
                         empHrs = 8;
                         break;
+
                     default:
                         empHrs = 0;
                         break;
 
                 }
-
                
                 totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Days :\n" + totalWrkDays + "Employment Hours : " + empHrs);
+                Console.WriteLine("Days :" + totalWrkDays + "Employment Hours : " + empHrs);
+
+
             }
             totalEmpWage = totalEmpHrs + empRateHrs;
-            Console.WriteLine("Total Emp Wage is : " + totalEmpWage);
-            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Total Emp Wagefor company " + company + " is : " + totalEmpWage);
             return totalEmpWage;
 
         }
-
         static void Main(string[] args)
         {
-
-            UCaseMethod();
-
-
+           
+            UCEwage("DMART", 20, 2, 10);
+            UCEwage("RELIANCW", 10, 4, 20);
 
         }
     }
 
 }
-
